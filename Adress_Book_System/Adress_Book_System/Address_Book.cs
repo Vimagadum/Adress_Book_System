489f389e2b532fffef4691f9ac78f5753fbc11ff
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-20 13:10:51 +0530

    [Vinayak] adding use case 1 for initializing variables of adresss book

---
+++ Adress_Book_System/Adress_Book_System/Address_Book.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adress_Book_System
+{
+    // Created class 
+    public class Address_Book
+    {
+        // Declaring variables
+        string firstname, lastname, state, city, address, email;
+        int zipcode;
+        long mobilenumber;
+
+        //Creating a constructor ton initialize variables
+        public Address_Book(string firstname, string lastname, string state, string city, string address, string email, int zipcode, long mobilenumber)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.state = state;
+            this.city = city;
+            this.address = address;
+            this.email = email;
+            this.zipcode = zipcode;
+            this.mobilenumber = mobilenumber;
+            
+        }
+
+        //Implimenting seter and geter
+        public string GetFirstName()
+        {
+            return firstname;
+        }
+        public void SetFirstName(string firstname)
+        {
+           this.firstname = firstname;
+        }
+        public string GetLastName()
+        {
+            return lastname;
+        }
+        public void SetLastName(string lastname)
+        {
+           this.lastname = lastname;
+        }
+        public string GetState()
+        {
+            return state;
+        }
+        public void SetState(string state)
+        {
+            this.state = state;
+        }
+        public string GetCity()
+        {
+            return city;
+        }
+        public void SetCity(string city)
+        {
+           this.city = city;
+        }
+        public string GetAddress()
+        {
+            return address;
+        }
+        public void SetAddress(string address)
+        {
+           this.address = address;
+        }
+        public string GetEmail()
+        {
+            return email;
+        }
+        public void SetEmail(string email)
+        {
+           this.email = email;
+        }
+        public int GetZipCode()
+        {
+            return zipcode;
+        }
+        public void SetZipCode(int zipcode)
+        {
+           this.zipcode = zipcode;
+        }
+        public long GetMobileNumber()
+        {
+            return mobilenumber;
+        }
+        public void SetMobileNumber(long mobilenumber)
+        {
+            this.mobilenumber = mobilenumber;
+        }
+
+        //Overrideing string method 
+        public override string ToString()
+        {
+            return ("First Name: " + firstname + " Last Name: " + lastname + " City: " + city + " State: " + state + " Address" + address + " zip: " + zipcode + " Phone Number: " + mobilenumber);
+        }
+
+    }
+}
---
+++ Adress_Book_System/Adress_Book_System/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Adress_Book_System;
+
+Console.WriteLine("WELCOME TO ADRESS BOOK SYSTEM");
+
+
+
+
+
