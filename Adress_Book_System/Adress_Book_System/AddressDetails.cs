891de6df123e50ed53758925dcbc821659250911
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-23 21:57:46 +0530

    [Vinayak] refactor to add multiple address book

--- Adress_Book_System/Adress_Book_System/AddressDetails.cs
+++ Adress_Book_System/Adress_Book_System/AddressDetails.cs
@@ -10,127 +10,21 @@ namespace Adress_Book_System
     {
         public char y;
         public char n;
-        ArrayList allcontacts = new ArrayList();
-        public void AddDetails()
+        //Dictonary to add address book to dictionary    
+        Dictionary<string, Address_Book>  addressBookDict = new Dictionary<string, Address_Book>();
+        public void AddNewAddressBook(string addressName, Address_Book addressBook)
         {
-            //Taking Input of Address Details
-
-            Console.WriteLine("Enter First Name");
-            string firstname = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            string lastname = Console.ReadLine();
-            Console.WriteLine("Enter State name");
-            string state = Console.ReadLine();
-            Console.WriteLine("Enter City Name");
-            string city = Console.ReadLine();
-            Console.WriteLine("Enter Address ");
-            string address = Console.ReadLine();
-            Console.WriteLine("Enter Email");
-            string email = Console.ReadLine();
-            Console.WriteLine("Enter Zip Code");
-            int zipcode = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Mobile Number");
-            long mobilenumber = Convert.ToInt64(Console.ReadLine());
-
-            // storing the details in person
-            Address_Book contacts = new Address_Book(firstname, lastname, state, city, address, email, zipcode, mobilenumber);
-                        //adding contacts in list
-            allcontacts.Add(contacts);
-            Console.WriteLine("Contact is Added");
-            view();
-
+            addressBookDict.Add(addressName, addressBook);
         }
-
-        //Writing method to Display all contacts
-        public void view()
+        //Method to fetch single address book if it matches user input
+        public Address_Book GetAddressBook(string name)
         {
-            foreach (Address_Book contactList in allcontacts)
+            foreach(var item in addressBookDict)
             {
-                Console.WriteLine(contactList);
+                if (item.Key== name)
+                    return item.Value;
             }
-        }
-
-        //Creating method to Edit the Contact
-        public void EditContact()
-        {
-            //Taking first name as input from the user to check weather this name is existing in contact list or not
-            Console.WriteLine("Enter the First Name of your contact that which contact you wants to Edit");
-            string Fname = Console.ReadLine();
-            foreach (Address_Book eachcontact in allcontacts)
-            {
-                //Comparing existing firts name to user entered first name
-                if (Fname == eachcontact.GetFirstName())
-                {
-                    Console.WriteLine("Enter First Name");
-                    string firstname = Console.ReadLine();
-
-                    //Here Replacing Old contact to new details
-                    eachcontact.SetFirstName(firstname);
-
-                    Console.WriteLine("Enter Last Name");
-                    string lastname = Console.ReadLine();
-                    eachcontact.SetLastName(lastname);
-                    Console.WriteLine("Enter State name");
-                    string state = Console.ReadLine();
-                    eachcontact.SetState(state);
-                    Console.WriteLine("Enter City Name");
-                    string city = Console.ReadLine();
-                    eachcontact.SetCity(city);
-                    Console.WriteLine("Enter Address ");
-                    string address = Console.ReadLine();
-                    eachcontact.SetAddress(address);
-                    Console.WriteLine("Enter Email");
-                    string email = Console.ReadLine();
-                    eachcontact.SetEmail(email);
-                    Console.WriteLine("Enter Zip Code");
-                    int zipcode = int.Parse(Console.ReadLine());
-                    eachcontact.SetZipcode(zipcode);
-                    Console.WriteLine("Enter Mobile Number");
-                    long mobilenumber = Convert.ToInt64(Console.ReadLine());
-                    eachcontact.Setmobilenumber(mobilenumber);
-                    break;
-                    Console.WriteLine("Contact is updated");
-                    view();
-
-
-
-                }
-                else
-                {
-                    Console.WriteLine("Invalid contact name please try again");
-
-                }
-            }
-
-        }
-
-        //Creating method to Delete Contact
-        public void DeleteContact()
-        {
-            //Taking first name as input from the user to check weather this name is existing in contacts list or not
-            Console.WriteLine("Enter the First Name of your contact that which contact you wants to Delete");
-            string Fname = Console.ReadLine();
-            foreach (Address_Book eachcontact in allcontacts)
-            {
-               //Comparing existing firts name to user entered first name
-                if (Fname == eachcontact.GetFirstName())
-                {
-                    Console.WriteLine("Do you really want to Delete this contact?? type y/n");
-                    string key = Console.ReadLine();
-                    if(key == "y")
-                    {
-                        //Removing Contact from the allcontacts list
-                        allcontacts.Remove(eachcontact);
-                        Console.WriteLine("Contact is Deleted");
-                        break;
-                        view();
-                    }
-                    else
-                    {
-                        Console.WriteLine("Contact does not exist please enter valid name");
-                    }
-                }
-            }
+            return null;
         }
     }
 }
