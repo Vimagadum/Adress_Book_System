e4518fce90f09a899cdb38e15d7edd9d9f81b5f3
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-02-25 17:13:56 +0530

    [Vinayak] reading file from json

---
+++ Adress_Book_System/AddressBook_RestSharp/AddressBook_RestSharp.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace AddressBook_RestSharp
+{
+    public class Contacts
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+    }
+    [TestClass]
+    public class AddressBook_RestSharp
+    {
+        
+        RestClient client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            client = new RestClient("http://localhost:3000");
+        }
+        private RestResponse GetAddressList()
+        {
+            // arrange
+            RestRequest request = new RestRequest("/AddressBook", Method.Get);
+
+            // act
+            RestResponse response = client.ExecuteAsync(request).Result;
+            return response;
+        }
+
+        [TestMethod]
+        public void OnCallingGETApi_ReturnAddressList()
+        {
+            RestResponse response = GetAddressList();
+
+            // assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<AddressBook_RestSharp> dataResponse = JsonConvert.DeserializeObject<List<AddressBook_RestSharp>>(response.Content);
+            Assert.AreEqual(1, dataResponse.Count);
+        }
+    }
+}
---
+++ Adress_Book_System/AddressBook_RestSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AddressBook_RestSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("AddressBook_RestSharp")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("589efe18-b050-4747-8862-e4e186e4b9dc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
